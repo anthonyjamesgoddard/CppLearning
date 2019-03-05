464219d6dd41709ea1626e4475bb98489ea55b7b
anthonyjamesgoddard <antg3254@gmail.com>
2019-03-05 20:46:42 +0000

    done some work

---
+++ Euler18.cpp
@@ -0,0 +1,4 @@
+// going to use boost graph to solve problem 18
+// we can cast this problem as a graph problem.
+// the numbers can be thought of as weights on
+// a path.
--- workingwithstrings.cpp
+++ workingwithstrings.cpp
@@ -16,20 +16,51 @@ bool isPalindromic(const std::string& s)
 
 std::string intToString(int x)
 {
+	bool is_negative = false;
+	if(x < 0)
+	{
+		is_negative = true;
+	}
+	
+	std::string s;
+	
+	do {
+		s += '0' + abs(x % 10);
+		x /= 10;
+	} while(x);
+
+	if(is_negative) {
+		s += '-';
+	}
+
+	return {s.rbegin(), s.rend()};
 	
 }
 
-int stringToInt(std::string& s)
+// slightly more elegant than what I come up with
+int stringToInt(const std::string& s)
 {
-	
+	int result = 0;
+	for(int i = s[0] == '-' ? 1 : 0; i < s.size(); ++i)
+	{
+		const int digit = s[i] - '0';
+		result = result * 10 + digit;
+	}
+	return s[0] == '-' ? -result : result;
 }
 
 
+std::string convertBase(const std::string& NumAsString, int b1, int b2)
+{
+	bool is_negative = NumAsString
+	
+}
 
 int main()
 {
-	std::string str = "helleh";
+	std::string str = "100001";
 	std::cout << str << std::endl;
-	std::cout << isPalindromic(str) << std::endl;
+	std::cout << intToString(10) << std::endl;
+	std::cout << stringToInt(str) << std::endl;
 	return 0;
 }
