341fec06990293e01a1875dd3500f6efa8eeecdd
anthonyjamesgoddard <antg3254@gmail.com>
2019-03-07 20:10:19 +0000

    update

--- workingwithstrings.cpp
+++ workingwithstrings.cpp
@@ -48,19 +48,36 @@ int stringToInt(const std::string& s)
 	}
 	return s[0] == '-' ? -result : result;
 }
+std::string ConstructFromBase(int NumAsInt, int base)
+{
+	return NumAsInt == 0 ? "" : ConstructFromBase(NumAsInt / base, base) +
+		(char)(NumAsInt % base >= 10 ? 'A' + NumAsInt % base - 10
+			   : '0' + NumAsInt % base);
+}
+
 
 
 std::string convertBase(const std::string& NumAsString, int b1, int b2)
 {
 	bool is_negative = NumAsString.front() == '-';
-	
+	int NumAsInt = 0;
+	for(size_t i = (is_negative == true ? 1 : 0);
+		i < NumAsString.size();
+		i++)
+	{
+		NumAsInt *= b1; 
+		NumAsInt += isdigit(NumAsString[i]) ? NumAsString[i] - '0' : NumAsString[i] - 'A' + 10;
+		std::cout << NumAsInt << std::endl;
+	}
+	return (is_negative ? "-" : "") +
+		(NumAsInt == 0 ? "0" : ConstructFromBase(NumAsInt, b2));
 }
+
+
 
 int main()
 {
-	std::string str = "100001";
-	std::cout << str << std::endl;
-	std::cout << intToString(10) << std::endl;
-	std::cout << stringToInt(str) << std::endl;
-	return 0;
+	std::string str = "100000";
+	std::cout << convertBase(str,2,10) << std::endl;
+  	return 0;
 }
