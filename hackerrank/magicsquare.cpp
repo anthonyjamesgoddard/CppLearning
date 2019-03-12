2c5054f93741ff9f2a2d749ec32e6cb3a3a5bb82
anthonyjamesgoddard <antg3254@gmail.com>
2019-03-12 20:25:32 +0000

    magic squares

---
+++ hackerrank/magicsquare.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+int difference(std::vector<std::vector<int>>& s, std::vector<std::vector<int>>& t)
+{
+    int diff = 0, temp;
+    for(int i=0;i<3;i++){
+        for(int j = 0; j < 3;j++){
+            temp = s[i][j]-t[i][j];
+            diff += (temp >=0 ) ? temp : -temp; 
+        }
+    }
+    return diff;
+}
+
+int main()
+{
+    std::vector<std::vector<std::vector<int>>> magics = {
+        {{8,1,6},{3,5,7},{4,9,2}},
+        {{4,3,8},{9,5,1},{2,7,6}},
+        {{2,9,4},{7,5,3},{6,1,8}},
+        {{6,7,2},{1,5,9},{8,3,4}},
+        {{6,1,8},{7,5,3},{2,9,4}},
+        {{8,3,4},{1,5,9},{6,7,2}},
+        {{4,9,2},{3,5,7},{8,1,6}},
+        {{2,7,6},{9,5,1},{4,3,8}}};
+    std::vector<std::vector<int>> magic = {{2,7,6},{9,5,1},{4,3,8}};
+
+    std::vector<std::vector<int>> input = {{4,5,8},{2,4,1},{1,9,7}};
+
+    int min = 1000000;
+    for(auto&v:magics){
+        auto temp = difference(v,input);
+        if(temp < min) min = temp;
+    }
+    
+    std::cout << min << std::endl;
+    return 0;
+}
