2eadb6e746a4b14b537e9c4b76f829a8b3e15b13
anthonyjamesgoddard <antg3254@gmail.com>
2019-03-12 21:02:36 +0000

    next one

--- hackerrank/pickingNumbers.cpp
+++ hackerrank/pickingNumbers.cpp
@@ -1,6 +1,19 @@
-#include<iostream>
-
-int main()
-{
-	
+int pickingNumbers(vector<int> a) {
+    sort(a.begin(),a.end());
+    int counter = 0;
+    int chosen = a[0];
+    std::vector<int> counters;
+    for(auto&v:a){
+        if(abs(v-chosen) <= 1){
+            counter++;
+        }
+        else{
+            counters.push_back(counter);
+            counter = 1;
+            chosen = v;
+        }
+    }
+    return *max_element(counters.begin(),counters.end());
 }
+
+// failing test wherer ther are 100 elements with same value
