a9e2023cc09b7157e62c8de2fe822a9801df25df
Anthony Goddard <40886470+anthonyjamesgoddard@users.noreply.github.com>
2019-03-19 20:33:12 +0000

    Started posh solution to between sets

---
+++ hackerrank/BetweenSets.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+// function that returns the primes in the prime factorisation of a number
+// and the power to which they occur.
+std::vector<std::pair<int, int>> primeFactors(int n)
+{
+	while (n % 2 == 0) {
+
+	}
+}
+
+// function that finds the least common multiple of a set.
+// we can do this by picking out the largest index for each prime
+// factor for all the numbers in the set.
+int leastCommonMultiple()
+{
+	return 0;
+}
+
+// function that finds the greatest common divisor of a set
+
+
+
+int main()
+{
+	/*
+		1. Find the LCM of A, LCM_A. Any other multiple of A is a multiple of LCM_A.
+		2. Find the GCD of B, GCD_B. All other common divisors of B are less than GCD_B.
+		3. Find how many divisors are in GCD_B/LCM_A
+
+		why does this work?
+	*/
+}
