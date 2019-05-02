4011be69c3ee58a0b2b02183a744c020285bb27b
Anthony Goddard <40886470+anthonyjamesgoddard@users.noreply.github.com>
2019-05-02 22:58:00 +0100

    Add files via upload

--- moveSemanticsexample.cpp
+++ moveSemanticsexample.cpp
@@ -1,33 +1,62 @@
- // move semantics 
- // influenced by the introduction of rvalues
- //
- 
+#include<iostream>
+#include<list>
+
 class BigVec 
 {
 private:
 	int     	m_size;
 	double*     m_data;
 public:
-	BigVec() : m_size{0},
-			   m_data(nullptr) {};	
-
 	// copy constructor
-	BigVec(BigVec& bv) : m_size(bv.m_size) {
+	BigVec(const BigVec& bv) : m_size(bv.m_size) {
+		std::cout << "copy called" << std::endl;
 		m_data = new double[bv.m_size];
-		for(int i = 0; i < m_size; i++) {
-			m_data[i] = bv.m_data[i];
-		}
+		for(int i=0; i<m_size; i++) { m_data[i] = bv.m_data[i]; }
 	}
 
 	// move contructor
 	BigVec(BigVec&& bv) : m_size(bv.m_size), 
 						  m_data(bv.m_data) {
+		std::cout << "move called" << std::endl;
 		bv.m_data = nullptr;
 	}
 
-	~BigVec(){
-		if(!m_data) delete m_data;
+	// move assignment operator
+	BigVec& operator=(BigVec&& bv) {
+		std::cout << "sdfsdf" << std::endl;
+		if(this == &bv) return *this; // self referential ?
+		delete m_data; 				  // delete current data
+		m_data = bv.m_data;
+		m_size = bv.m_size;
+		// reset object as resources have been moved
+		bv.m_data = nullptr;
+		bv.m_size = 0;
+		return *this;
 	}
+
+	BigVec(int size) : m_size(size) {
+		std::cout << "toy " << std::endl;
+		m_data = new double[size];
+		for(int i = 0; i < size; i++) m_data[i] = 0;
+	}
+
+	~BigVec() { delete m_data; }
 };
 
+BigVec CreateBigVec() {
+	return BigVec(100);
+}
 
+int main()
+{
+	// copy constructor is called
+	auto v = CreateBigVec();
+	BigVec vec(v);
+
+	// move constructor is called
+	std::list<BigVec> listOfBigVecs;
+	listOfBigVecs.push_back(CreateBigVec());
+
+
+	return 0;
+}
